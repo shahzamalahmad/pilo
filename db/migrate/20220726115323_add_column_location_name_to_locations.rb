64a522657cf6e0988fbf8d3d1922c2943362c96e
shahzamalahmad <shahzamal2013@gmail.com>
2022-09-04 15:18:29 +0530

    aaa

--- db/migrate/20220726115323_add_column_location_name_to_locations.rb
+++ db/migrate/20220726115323_add_column_location_name_to_locations.rb
@@ -1,5 +1,5 @@
 class AddColumnLocationNameToLocations < ActiveRecord::Migration[7.0]
   def change
-    add_column :locations, :location_name, :string
+    # add_column :locations, :location_name, :string
   end
 end
